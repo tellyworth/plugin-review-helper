9fd135a1b9a711fb72e5cd8ea8cd5c57c8854c84
tellyworth <alex@automattic.com>
2024-01-09 17:12:11 +1100

    Simple direct link to log file, and phpinfo

--- plugin-review-helper.php
+++ plugin-review-helper.php
@@ -26,14 +26,28 @@ ini_set( 'display_errors', 1 );
 ini_set( 'log_errors', 1 );
 ini_set( 'html_errors', 1 );
 // Put the log file somewhere accessible both by code and web.
-#ini_set( 'error_log', PRH_LOG_FILE );
-set_error_handler( __NAMESPACE__ . '\prh_error_handler', E_ALL );
-register_shutdown_function( __NAMESPACE__ . '\prh_shutdown_handler' );
+ini_set( 'error_log', PRH_LOG_FILE );
 
 add_action( 'admin_menu', __NAMESPACE__ . '\prh_admin_menu' );
 
 function prh_admin_menu() {
 	add_menu_page( 'Plugin Review Helper', 'Plugin Review Helper', 'manage_options', 'plugin-review-helper', __NAMESPACE__ . '\prh_admin_page' );
+}
+
+add_action( 'admin_init', __NAMESPACE__ . '\prh_admin_init' );
+
+function prh_admin_init() {
+	if ( 'wasm' !== strtolower( php_sapi_name() ) ) {
+		add_action( 'admin_notices', __NAMESPACE__ . '\prh_admin_notice' );
+	}
+}
+
+function prh_admin_notice() {
+	?>
+	<div class="notice notice-error">
+		<p>Plugin Review Helper is intended for use within a WASM environment, not <code><?php echo esc_html( php_sapi_name() ); ?></code>.</p>
+	</div>
+	<?php
 }
 
 function prh_admin_page() {
@@ -42,26 +56,13 @@ function prh_admin_page() {
 		<h1>Plugin Review Helper</h1>
 		<p>Plugin Review Helper is a plugin for reviewing plugins. It makes a plugin moderator's job a little easier. It is intended for use within Playground.</p>
 		<p>Plugin Review Helper is a work in progress. It is not yet ready for use.</p>
+		<p>Error log: <a href="<?php echo esc_url( WP_CONTENT_URL . '/plugin-review-helper.log' ); ?>"><?php echo esc_url( PRH_LOG_FILE ); ?></a></p>
+
+		<p>phpinfo(): <a href="<?php echo esc_url( admin_url( 'admin.php?page=plugin-review-helper&phpinfo=1' ) ); ?>">View</a></p>
 	</div>
 	<?php
 
-	global $menu;
-	var_dump( $menu );
-
-	$lines = array_reverse( file( PRH_LOG_FILE ) );
-
-	echo '<table class="widefat striped">';
-	echo '<tr><th>Time</th><th>Error</th><th>File</th><th>Line</th><th>What</th><th style="width:70%">Message</th></tr>';
-	foreach ( $lines as $line ) {
-		$data = json_decode( trim($line) );
-		echo '<tr>';
-		echo '<td>' . $data[0] . '</td>';
-		echo '<td>' . prh_errno_to_string( $data[1] ) . '</td>';
-		echo '<td>' . $data[3] . '</td>';
-		echo '<td>' . $data[4] . '</td>';
-		echo '<td>' . ( $data[5] ?? '' ) . '</td>';
-		echo '<td style="overflow:scroll"><pre>' . esc_html($data[2]) . '</pre></td>';
-		echo '</tr>';
+	if ( isset( $_GET['phpinfo'] ) ) {
+		phpinfo();
 	}
-	echo '</table>';
 }
